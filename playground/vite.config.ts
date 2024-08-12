c08908ce6a1875fa5ffb93a62cc1057771bb178f
严浩 <me@yanhao.ren>
2024-08-12 11:02:12 +0800

    chore: Remove unused dependencies and plugins

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,27 +1,18 @@
 import Vue from '@vitejs/plugin-vue';
-import Markdown from 'unplugin-vue-markdown/vite';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+
 export default defineConfig({
   clearScreen: false,
   build: {
     sourcemap: true,
   },
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    vueJsx(),
-    Markdown({}),
-    // VueDevtools(),
-    Inspect(),
-  ],
+  plugins: [Vue(), vueJsx(), Inspect()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./src', import.meta.url)),
       '@yanhao98/vue-router-helper': fileURLToPath(new URL('../src', import.meta.url)),
     },
   },
